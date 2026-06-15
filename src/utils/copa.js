3f9ee4cb4aeb01b51bfa8812fa464dc6af251ae5
DaviLLi <davilandal10@gmail.com>
2026-06-15 20:58:50 -0300

    Adiciona testes unitários da aplicação

---
+++ src/utils/copa.js
@@ -0,0 +1,17 @@
+function nomeProjeto() {
+  return "Copa 2026 Explorer";
+}
+
+function anoFinal() {
+  return 2026;
+}
+
+function paisesSede() {
+  return ["Estados Unidos", "Canadá", "México"];
+}
+
+module.exports = {
+  nomeProjeto,
+  anoFinal,
+  paisesSede,
+};
